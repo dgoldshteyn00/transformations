7ecbaa1c0afbbcebdf34cc6a479a1988867eeb47
dgoldshteyn00 <45824229+dgoldshteyn00@users.noreply.github.com>
2020-03-02 10:58:57 -0500

    Update draw.py

--- draw.py
+++ draw.py
@@ -9,10 +9,10 @@ def draw_lines(matrix, screen, color):
 
     point = 0
     while point < len(matrix) - 1:
-        draw_line(matrix[point][0],
-                  matrix[point][1],
-                  matrix[point + 1][0],
-                  matrix[point + 1][1],
+        draw_line(int(matrix[point][0]),
+                  int(matrix[point][1]),
+                  int(matrix[point + 1][0]),
+                  int(matrix[point + 1][1]),
                   screen, color)
         point += 2
 
@@ -26,20 +26,86 @@ def add_point(matrix, x, y, z=0):
     matrix.append([x, y, z, 1])
 
 
-def draw_line(x0, y0, x1, y1, screen, color):  # this version uses error calculations
-    dx = abs(x1 - x0)
-    mx = 1 if x0 < x1 else -1
-    dy = -abs(y1 - y0)
-    my = 1 if y0 < y1 else -1
-    error = dx + dy
-    while 1 == 1:  # for i in range(1000): <test code>
-        plot(screen, color, x0, y0)
-        if x0 == x1 and y0 == y1:
-            break
-        error_2 = 2 * error
-        if error_2 >= dy:
-            error += dy
-            x0 += mx
-        if error_2 <= dx:
-            error += dx
-            y0 += my
+def draw_line(x0, y0, x1, y1, screen, color):
+    # swap points if going right -> left
+    if x0 > x1:
+        xt = x0
+        yt = y0
+        x0 = x1
+        y0 = y1
+        x1 = xt
+        y1 = yt
+
+    x = x0
+    y = y0
+    A = 2 * (y1 - y0)
+    B = -2 * (x1 - x0)
+
+    # octants 1 and 8
+    if (abs(x1 - x0) >= abs(y1 - y0)):
+
+        # octant 1
+        if A > 0:
+            d = A + B / 2
+
+            while x < x1:
+                plot(screen, color, x, y)
+                if d > 0:
+                    y += 1
+                    d += B
+                x += 1
+                d += A
+            # end octant 1 while
+            plot(screen, color, x1, y1)
+        # end octant 1
+
+        # octant 8
+        else:
+            d = A - B / 2
+
+            while x < x1:
+                plot(screen, color, x, y)
+                if d < 0:
+                    y -= 1
+                    d -= B
+                x += 1
+                d += A
+            # end octant 8 while
+            plot(screen, color, x1, y1)
+        # end octant 8
+    # end octants 1 and 8
+
+    # octants 2 and 7
+    else:
+        # octant 2
+        if A > 0:
+            d = A / 2 + B
+
+            while y < y1:
+                plot(screen, color, x, y)
+                if d < 0:
+                    x += 1
+                    d += A
+                y += 1
+                d += B
+            # end octant 2 while
+            plot(screen, color, x1, y1)
+        # end octant 2
+
+        # octant 7
+        else:
+            d = A / 2 - B;
+
+            while y > y1:
+                plot(screen, color, x, y)
+                if d > 0:
+                    x += 1
+                    d += A
+                y -= 1
+                d -= B
+            # end octant 7 while
+            plot(screen, color, x1, y1)
+        # end octant 7
+    # end octants 2 and 7
+# end draw_line
+
